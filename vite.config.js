4fed028b985349f8e348cf6d7a9e320d56650f17
cateiru <yuto.w51942@gmail.com>
2024-02-25 14:19:25 +0900

    コメント追加

--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,11 @@ export default {
   },
   plugins: [
     {
+      // Chromeでは公開ウェブサイトからプライベートネットワークエンドポイントへの直接アクセスをする場合、
+      // `Access-Control-Request-Private-Network: true` と `Access-Control-Allow-Private-Network: true` ヘッダを追加する
+      // 必要がある。
+      // ref. https://developer.chrome.com/blog/private-network-access-preflight
+      // ref2. https://github.com/vitejs/vite/discussions/7134#discussioncomment-3422554
       name: "configure-response-headers",
       configureServer: (server) => {
         server.middlewares.use((_req, res, next) => {
